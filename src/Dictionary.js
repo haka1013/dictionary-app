ddbda8dbaeae2b05b5e20bb79a8d4243cfec8a36
haka1013 <katha_haas@web.de>
2023-06-17 22:07:08 +0200

    Display meanings

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,42 +1,34 @@
 import React, { useState } from "react";
 import axios from "axios";
+import Results from "./Results";
 import "./Dictionary.css";
 
 export default function Dictionary() {
   const [keyword, setKeyword] = useState("");
-  const [loaded, setLoaded] = useState(false);
+  const [results, setResults] = useState(null);
 
   function handleResponse(response) {
-    console.log(response.data.meanings[0]);
-  }
-
-  function search(keyword) {
-    let apiKey = `f552o2btc343e2d6edd4e830ffa6cab0`;
-    let url = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=${apiKey}`;
-    axios.get(url).then(handleResponse);
-    setLoaded(true);
+    setResults(response.data);
   }
 
   function handleSubmit(event) {
     event.preventDefault();
-    search(keyword);
+    let apiKey = `f552o2btc343e2d6edd4e830ffa6cab0`;
+    let url = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=${apiKey}`;
+    axios.get(url).then(handleResponse);
   }
 
   function handleKeywordChange(event) {
     setKeyword(event.target.value);
   }
 
-  if (loaded) {
-    return (
-      <div className="Dictionary">
-        <form onSubmit={handleSubmit}>
-          <input type="search" onChange={handleKeywordChange} />
-          <input type="submit" />
-        </form>
-      </div>
-    );
-  } else {
-    search(`sunrise`);
-    return "Loading";
-  }
+  return (
+    <div className="Dictionary">
+      <form onSubmit={handleSubmit}>
+        <input type="search" onChange={handleKeywordChange} />
+        <input type="submit" />
+      </form>
+      <Results results={results} />
+    </div>
+  );
 }

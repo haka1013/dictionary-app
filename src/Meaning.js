ea7fabba55edb8a5dc21af00d6d549f373a43095
haka1013 <katha_haas@web.de>
2023-06-18 15:46:44 +0200

    Included synonyms

--- src/Meaning.js
+++ src/Meaning.js
@@ -1,14 +1,15 @@
 import React from "react";
+import Synonyms from "./Synonyms";
 
 export default function Meaning(props) {
-  console.log(props.meaning);
   return (
     <div className="Meaning">
       <h4>{props.meaning.partOfSpeech}</h4>
-      <p>{props.meaning.definition}</p>
+      <p>Definition: {props.meaning.definition}</p>
       <p>
-        <em>{props.meaning.example}</em>
+        Example: <em>{props.meaning.example}</em>
       </p>
+      <Synonyms synonyms={props.meaning.synonyms} />
     </div>
   );
 }

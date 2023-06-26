4c70692195426ff9c6215025fdd8456292a2ca70
haka1013 <katha_haas@web.de>
2023-06-26 09:46:23 +0200

    Finishing css-touches

--- src/App.js
+++ src/App.js
@@ -6,13 +6,24 @@ export default function App() {
   return (
     <div className="App">
       <div className="container">
+        <header>
+          <img src="magic_wand.svg" alt="magic wand" />
+        </header>
         <main>
           <h1>Kat's Dictionary</h1>
           <h5>Type a word to see some magic happen ✨</h5>
           <Dictionary />
         </main>
         <footer>
-          This app was coded by Kat Haas 🐰 and is{" "}
+          This app was coded by{" "}
+          <a
+            href="https://www.linkedin.com/in/katharina-haas-283675174/"
+            rel="noreferrer"
+            target="blank"
+          >
+            Kat Haas
+          </a>{" "}
+          🐰 and is{" "}
           <a
             href="https://github.com/haka1013/dictionary-app"
             rel="noreferrer"
